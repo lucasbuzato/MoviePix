f4b44b12f985341244c41b8453ac98cadd836ad2
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-21 09:34:59 -0300

    MoviePix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,9 +28,9 @@ function Home() {
       <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
         <div class="flex flex-nowrap  ml-2 ">
           <div class="inline-block px-3">
-            <div
-              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            ></div>
+            <div class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+              
+            </div>
           </div>
           <div class="inline-block px-3">
             <div
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,14 @@ export default {
       'Red': '#4B1411',
       'Red-claro':'#F94239',
       'Escuro':'#1E1D1D'
+      
     },
     fontFamily: {
       customFont: ['"Russo One"', "sans-serif"],
     },
+    backgroundImage: {
+      'footer-texture': "url('/src/componentes/Images/pexels-donaldtong94-109669.jpg')",
+    }
   },
   },
   plugins: [],
